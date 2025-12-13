875647a52926d2589fbc3c36daba81d78775f18d
devjorginho <jcarvalho.swe@gmail.com>
2025-12-13 16:17:22 +0000

    Auto commit via 'bye' 🚀

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,11 +6,16 @@
 /*   By: jde-carv <jde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 14:54:53 by jde-carv          #+#    #+#             */
-/*   Updated: 2025/12/13 15:40:42 by jde-carv         ###   ########.fr       */
+/*   Updated: 2025/12/13 15:49:15 by jde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
+
+
+//ifstream
+//ofstream
 
 int main(int ac, char **av) {
 	if(ac != 4)
@@ -21,4 +26,7 @@ int main(int ac, char **av) {
 	std::string filename = av[1];
 	std::string s1 = av[2];
 	std::string s2 = av[3];
+	std::string line;
+
+	std::ifstream inFile{filename};
 }
